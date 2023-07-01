8ae1e5b035f1679060032b23b8adbf9fb5bf8095
zeem2626 <96663491+zeem2626@users.noreply.github.com>
2023-07-02 00:07:27 +0530

    Add files via upload

---
+++ src/frontend/App.js
@@ -0,0 +1,23 @@
+import React, {useState, createContext} from 'react'
+import "./App.css"
+import Calendar from "./Calendar"
+import Days from "./Days"
+import AttendanceContextProvider from "../firebase/AttendanceContextProvider.js"
+import CalendarMonthInfo2 from './CalendarMonthInfo2'
+
+const App = () => { 
+
+  return (
+  <div>
+
+      <AttendanceContextProvider >
+        <Calendar />
+      </AttendanceContextProvider >
+
+      <CalendarMonthInfo2 years="2023" months="7" />
+
+  </div>
+  )
+}
+
+export default App
---
+++ src/frontend/CalendarDates.js
@@ -0,0 +1,93 @@
+import React, { useEffect, useState, useContext } from "react";
+import { attendanceDataContext } from "../firebase/AttendanceContextProvider.js";
+import CalendarMonthInfo from "./CalendarMonthInfo.js";
+
+const CalendarDates = ({selectedDate}) => {
+  const {space, numberOfDays} = CalendarMonthInfo(selectedDate.year, selectedDate.month);
+//   console.log(space)
+//   console.log(numberOfDays)
+   const spaceArray = Array(space).fill("");
+   const dateArray = [...spaceArray, ...numberOfDays];
+
+   let i = 0;
+
+  //  const [dateArray, setDate] = useState(date);
+
+   useEffect(() => {
+      // setDate([...spaceArray, ...dateArray]);
+    }, []);
+    
+    const attendanceData = useContext(attendanceDataContext);
+
+    return (
+      <div className="calendar_dates">
+         {/* <CalendarMonthInfo year={2023} month={4}/> */}
+         <ul>
+            {/* if only 0, 1, 2 stored in Array */}
+            {/* {dateArray.map((elem, index) =>(
+                (spaceArray.length > index) ? (
+                  <li key={index}>{`${elem}`}</li>
+                ) : (attendanceData[index - spaceArray.length] == 0) ? (
+                  <li className="absent" key={index}>
+                    <span>{elem}</span>
+                  </li>
+                ) : (attendanceData[index - spaceArray.length] == 1) ? (
+                  <li className="attendanceData" key={index}>
+                    <span>{elem}</span>
+                  </li>
+                ) : (attendanceData[index - spaceArray.length] == 2) ? (
+                  <li className="holiday" key={index}>
+                    <span>{elem}</span>
+                  </li>
+                ) : (
+                  <li className="other" key={index}>
+                  <span>{elem}</span>
+                  </li>
+                  )
+                ))} */}
+
+            {/* if date and its attendace stored in Array */}
+            {dateArray.map((elem, index) => {
+               if (index < spaceArray.length && attendanceData[i]) {
+                  return <li key={index}></li>;
+               }
+               // ###############3
+               else if (index >= spaceArray.length && attendanceData[i]) {
+                  if (elem == attendanceData[i].date && attendanceData[i].record) {
+                     i++;
+                     return (
+                        <li className="present" key={index}>
+                           <span>{elem}</span>
+                        </li>
+                     );
+                  } else if (elem == attendanceData[i].date && !attendanceData[i].record) {
+                     i++;
+                     return (
+                        <li className="absent" key={index}>
+                           <span>{elem}</span>
+                        </li>
+                     );
+                  } else {
+                     return (
+                        <li className="holiday" key={index}>
+                           <span>{elem}</span>
+                        </li>
+                     );
+                  }
+               }
+               // ###############3
+               else {
+                  return (
+                     <li className="other" key={index}>
+                        <span>{elem}</span>
+                     </li>
+                  );
+               }
+            })}
+            
+         </ul>
+      </div>
+   );
+};
+
+export default CalendarDates;
